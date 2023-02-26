5c4a5847b745bf3697c12f758933e02f01edc7d5
anshulgoyal43 <65175818+anshulgoyal43@users.noreply.github.com>
2023-02-26 23:02:29 +0530

    Add files via upload

---
+++ build/CMakeFiles/assignment-1.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/button.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/button.cpp.o"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/game.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/game.cpp.o"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/imposter.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/imposter.cpp.o"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/main.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/main.cpp.o"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/maze.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/maze.cpp.o"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/player.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/player.cpp.o"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/resource_manager.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/resource_manager.cpp.o"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/shader.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/shader.cpp.o"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/stb_image.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/stb_image.cpp.o"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/text_renderer.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/text_renderer.cpp.o"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/src/texture.cpp" "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/assignment-1.dir/src/texture.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLFW_INCLUDE_NONE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../libraries/glfw/include"
+  "../libraries/glad/include"
+  "../libraries/glm"
+  "/usr/local/include/freetype2"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/build/libraries/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake"
+  "/Users/tanish/Documents/Computer Graphics/assignments/1/build/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
